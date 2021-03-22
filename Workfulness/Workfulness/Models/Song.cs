5f4b7250468e961efc7cfc0ecf538d693e1e387e
Maria Gryglewska <maria.gryglewska51@gmail.com>
2021-03-22 13:07:08 +0100

    Added option for seletcing song to directly from the list

--- Workfulness/Workfulness/Models/Song.cs
+++ Workfulness/Workfulness/Models/Song.cs
@@ -7,9 +7,10 @@ namespace Workfulness.Models
 {
     public record Song
     {
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string SongUrl { get; set; }
+        public int SongId { get; init; }
+        public string Title { get; init; }
+        public string Author { get; init; }
+        public string SongUrl { get; init; }
 
         public string SongFullName { get { return $"{Author} - {Title}"; } }
     }
