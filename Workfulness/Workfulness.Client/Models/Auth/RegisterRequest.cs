74996876f55f734c185f976cc904000300fea85f
Maria Gryglewska <maria.gryglewska51@gmail.com>
2021-06-19 20:35:01 +0200

    Added registration of user

--- Workfulness/Workfulness.Client/Models/Auth/RegisterRequest.cs
+++ Workfulness/Workfulness.Client/Models/Auth/RegisterRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,15 @@ namespace Workfulness.Client.Models.Auth
 {
     public class RegisterRequest
     {
+        [Required]
         public string Firstname { get; set; }
+        [Required]
         public string Lastname { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string ConfirmationPassword { get; set; }
     }
 }
